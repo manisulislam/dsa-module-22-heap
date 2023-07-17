159d42b62d9b2296f62b6c62c03ee405b73430a8
anisul islam <anisulislam980340@gmail.com>
2023-07-17 21:39:51 +0600

    max and min heap solved

---
+++ insert_max_heap.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int curr_idx = v.size() - 1;
+
+        while (curr_idx != 0)
+        {
+            int parent_idx = (curr_idx - 1) / 2;
+            if (v[parent_idx] < v[curr_idx])
+                swap(v[parent_idx], v[curr_idx]);
+            else
+                break;
+            curr_idx = parent_idx;
+        }
+    }
+    for(int val: v){
+        cout<<val<<" ";
+    }
+
+    return 0;
+}
